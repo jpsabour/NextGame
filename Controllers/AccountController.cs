bfc9b7f7d4efeaddef42b197e6cc9942a09b25a2
John <jpsabour@gmail.com>
2021-11-17 11:58:18 -0800

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using Game2Play.DBModel;
+using Game2Play.Models;
+using System;
+using System.Web.Mvc;
+
+namespace Game2Play.Controllers
+{
+    public class AccountController : Controller
+    {
+        UserDBEntities objUserDBEntities = new UserDBEntities();
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            UserModel objUserModel = new UserModel();
+            return View(objUserModel);
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserModel objUserModel)
+        {
+            if (ModelState.IsValid)
+            {
+                User objUser = new DBModel.User
+                {
+                    CreatedOn = DateTime.Now,
+                    Email = objUserModel.Email,
+                    FirstName = objUserModel.FirstName,
+                    LastName = objUserModel.LastName,
+                    Password = objUserModel.Password
+                };
+                objUserDBEntities.Users.Add(objUser);
+                objUserDBEntities.SaveChanges();
+                return View();
+            }
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Game2Play.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "First Name is Required.")]
+        [Display(Name = "First Name: ")]
+        public string FirstName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Last Name is Required.")]
+        [Display(Name = "Last Name: ")]
+        public string LastName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email is Required.")]
+        [Display(Name = "Email: ")]
+        public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is Required.")]
+        [Display(Name = "Password: ")]
+        public string Password { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password-Confirmation is Required.")]
+        [Display(Name = "Confirm Password: ")]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage = "Password and Confirmation should be the same.")]
+        public string ConfirmPassword { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+    }
+}
